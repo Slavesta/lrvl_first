9444519b18d1b2b5ed3d0b2cffa3c602f965646c
Slava <vlarovina@gmail.com>
2019-02-10 15:50:39 -0800

    Add, Edit, Delete a User

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -45,6 +45,13 @@
                           <li><a href="{{route('admin.article.index')}}">Материалы</a></li>
                         </ul>
                       </li>
+                      <li class="dropdown mr-3">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Управление пользователями</a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="{{route('admin.user_managment.user.index')}}">Пользователи</a></li>
+
+                        </ul>
+                      </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
